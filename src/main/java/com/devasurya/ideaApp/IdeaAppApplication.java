ac130766e459c9dd409cb25fa1108b0bafae6597
R <devasurya.r@clarivate.com>
2021-05-24 11:57:20 +0530

    Initial commit

---
+++ src/main/java/com/devasurya/ideaApp/IdeaAppApplication.java
@@ -0,0 +1,13 @@
+package com.devasurya.ideaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaAppApplication.class, args);
+	}
+
+}
